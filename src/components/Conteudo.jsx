f5a0d710fb2a611aafd13a2469f35d23d7e2760b
ThiagoJO0705 <rm551624@fiap.com.br>
2023-09-04 09:25:25 -0300

    Iniciando a modularização no projeto Boyler-Plate.

---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,14 @@
+export default function Conteudo(){
+    return(
+        <>
+            <section>
+                <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consequuntur corrupti possimus omnis eaque vero? Molestias qui facilis adipisci! Rerum consequuntur fugiat sapiente cum voluptatibus ullam incidunt reiciendis sequi iure?</p>
+                <p>Perferendis et earum modi eveniet dolor blanditiis tempore pariatur minima ea ad. Vero obcaecati fugit distinctio incidunt maxime deserunt explicabo, dolorem quidem perferendis facere asperiores autem? Suscipit deserunt corporis vitae!</p>
+                <p>Odio voluptates quae laudantium cupiditate libero iure aut sed pariatur! Dolorum consequatur sequi excepturi quod delectus perspiciatis ab! Reprehenderit cumque labore obcaecati consequatur voluptatem hic esse eum ea ipsa non.</p>
+                <img src="./assets/react.svg" alt="Logo React" />
+                </div>
+            </section>
+        </>
+    )
+}
